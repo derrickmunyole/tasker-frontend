59a4a242c131a805970c922c492af2b40fb11efd
Derrick <derrickjust@outlook.com>
2024-08-01 00:42:36 +0300

    Added home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,43 +7,47 @@ function Home() {
     const beforeInjection = (svg) => {
         svg.classList.add('more-vert');
         svg.setAttribute('style', 'color: #fff');
-      }
-  return (
-    <div className='home-container'>
-        <div className="overview">
-            <div className="at-a-glance">
-                <div className="top">
-                    <p>Good evening, Adam</p>
-                    <ReactSVG src="/src/assets/icons/more_vert.svg"/>
+    }
+    return (
+        <div className='home-container'>
+            <div className="overview">
+                <div className="at-a-glance">
+                    <div className="top">
+                        <p>Good evening, Adam</p>
+                        <ReactSVG src="/src/assets/icons/more_vert.svg" />
+                    </div>
+                    <p className='h1'>At a glance</p>
+                    <div className="quick-stats">
+                        <div className="stat-item">
+                            <p>Total Projects</p>
+                        </div>
+                        <div className="stat-item">
+                            <p>Projects Completed</p>
+                        </div>
+                        <div className="stat-item">
+                            <p>Tasks due today</p>
+                        </div>
+                        <div className="stat-item">
+                            <p>Overdue tasks</p>
+                        </div>
+                    </div>
                 </div>
-                <p className='h1'>At a glance</p>
-                <div className="quick-stats">
-                    <div className="stat-item">
-                        <p>Total Projects</p>
+                <div className="activity-feed">
+                    <div className="activity-top">
+                        <p>Activity Feed</p>
+                        <ReactSVG src="/src/assets/icons/more_vert.svg" />
                     </div>
-                    <div className="stat-item">
-                        <p>Projects Completed</p>
-                    </div>
-                    <div className="stat-item">
-                        <p>Tasks due today</p>
-                    </div>
-                    <div className="stat-item">
-                        <p>Overdue tasks</p>
+                    <div className="feed">
+
                     </div>
                 </div>
             </div>
-            <div className="activity-feed">
-                <div className="activity-top">
-                    <p>Activity Feed</p>
-                    <ReactSVG src="/src/assets/icons/more_vert.svg" />
-                </div>
-                <div className="feed">
-                    
-                </div>
+            <div className='active-project-header'>
+                <h2>Active projects</h2>
+                <ReactSVG src="/src/assets/icons/keyboard_arrow_down.svg" />
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home

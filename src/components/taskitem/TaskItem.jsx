cad8b4cf6d8e2e326299baafbcbba02021f49b06
Derrick <derrickjust@outlook.com>
2024-08-16 23:42:22 +0300

    Added an event task action

--- src/components/taskitem/TaskItem.jsx
+++ src/components/taskitem/TaskItem.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { ReactSVG } from "react-svg";
 import './TaskItem.css'
 
-function TaskItem({ task, onActionClick }) {
+function TaskItem({ task, onActionClick, onDatePickerOpen }) {
   const [expanded, setExpanded] = useState(false);
 
   const toggleExpand = () => setExpanded(!expanded);
@@ -17,9 +17,13 @@ function TaskItem({ task, onActionClick }) {
         <div className="task-actions">
           <ReactSVG src="/src/assets/icons/create_new_folder.svg" onClick={() => onActionClick('addToProject', task)} />
           <ReactSVG src="/src/assets/icons/person_add.svg" onClick={() => onActionClick('assign', task)} />
-          <ReactSVG src="/src/assets/icons/notifications.svg" onClick={() => onActionClick('setDeadline', task)} />
+          <ReactSVG 
+            src="/src/assets/icons/notifications.svg" 
+            onClick={(e) => onDatePickerOpen(e, 'reminder', task.id)} 
+          />   
           <ReactSVG src="/src/assets/icons/repeat.svg" onClick={() => onActionClick('setRecurring', task)} />
           <ReactSVG src="/src/assets/icons/edit.svg" onClick={() => onActionClick('edit', task)} />
+          <ReactSVG src="/src/assets/icons/event.svg" onClick={() => onActionClick('event', task)} />
         </div>
       )}
     </div>
